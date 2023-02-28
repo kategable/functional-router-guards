359f3848d8a2ae451abc76f40de56352d79e4dc4
Kate Sky <kateskytech@gmail.com>
2023-02-28 14:47:53 -0600

    converted to inline function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,36 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { inject, NgModule } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  createUrlTreeFromSnapshot,
+  RouterModule,
+  Routes,
+} from '@angular/router';
+import { map } from 'rxjs';
 import { AccountComponent } from './account/account.component';
-import { authGuard } from './auth-guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { UserService } from './user.service';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'account', component: AccountComponent, canActivate: [authGuard] },
+  {
+    path: 'account',
+    component: AccountComponent,
+    canActivate: [
+      (next: ActivatedRouteSnapshot) => {
+        const service = inject(UserService);
+        return service
+          .isLoggedIn()
+          .pipe(
+            map((isLoggedIn) =>
+              isLoggedIn
+                ? true
+                : createUrlTreeFromSnapshot(next, ['/', 'login'])
+            )
+          );
+      },
+    ],
+  },
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
--- src/app/auth-guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { authGuard } from './auth-guard';
-
-describe('authGuard', () => {
-  it('should be created', () => {
-    expect(authGuard).toBeTruthy();
-  });
-});
